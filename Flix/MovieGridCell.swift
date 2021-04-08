8745da5ff1bbf33124748ac3fc0f6eb94a22135d
Hao Chiang <haomingchiang@gmail.com>
2021-04-07 20:40:20 -0700

    finished part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Hao-Ming Chiang on 4/5/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
